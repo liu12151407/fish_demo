f907e6990102573a70a3e68c653c99f99914424d
LIU YINGLONG <635536177@qq.com>
2019-11-07 14:24:30 +0800

    代码备份

--- lib/grid/effect.dart
+++ lib/grid/effect.dart
@@ -7,13 +7,13 @@ Effect<GridState> buildEffect() {
     GridAction.action: _onAction,
 
     ///初始化
-    Lifecycle.initState: _init,
+    Lifecycle.initState: _onLoadData,
   });
 }
 
 void _onAction(Action action, Context<GridState> ctx) {}
 
-void _init(Action action, Context<GridState> ctx) {
+void _onLoadData(Action action, Context<GridState> ctx) {
   ///发送事件
   ctx.dispatch(GridActionCreator.onLoadData());
 }
